1f4430706d735ce6a36d5be6805c0091ba8719c2
unknown <cogjava3103@iiht.tech>
2022-09-14 13:50:53 +0530

    new changes

--- src/main/java/com/author/controller/AuthorController.java
+++ src/main/java/com/author/controller/AuthorController.java
@@ -1,9 +1,15 @@
 package com.author.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.author.model.Author;
 import com.author.service.IAuthorService;
 import com.book.model.Book;
+import com.book.service.BookService;
 
 @RestController
 @RequestMapping("/digitalbooks")
@@ -20,9 +27,12 @@ public class AuthorController {
 	@Autowired
 	IAuthorService authorService;
 	
+	@Autowired
+	BookService bookService;
+	
 	@PostMapping("/createbook")
 	public Long createBook(@RequestBody Book book) {
-		Long id = authorService.createBook(book);
+		Long id = bookService.createBook(book);
 		return id;
 	}
 	@PostMapping("/author/signup")
@@ -30,4 +40,16 @@ public class AuthorController {
 		Long id = authorService.createAccount(author);
 		return id;
 	}
+	@GetMapping("/getallbooks")
+	public List<Book> getAllBooks(){
+		return bookService.getAllBooks();
+	}
+	
+	@PutMapping("/editbook")
+	public ResponseEntity<Book> editBook(@PathVariable("id") Long id ,@RequestBody Book book ){
+		
+	return  new ResponseEntity<Book>(bookService.editBook(book, id),HttpStatus.OK);
+		
+	}
+	
 }
--- src/main/java/com/author/service/IAuthorService.java
+++ src/main/java/com/author/service/IAuthorService.java
@@ -1,14 +1,15 @@
 package com.author.service;
 
+import java.util.List;
+
 import com.author.model.Author;
 import com.book.model.Book;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+@Component
 public interface IAuthorService  {
-
-	public Long createBook(Book book);
 	
 	public Long createAccount(Author author);
-	
-	public Book editBook(Book book,Long id);
 	
 }
