918c9b9997b1dfa6bb5f4d929ab3cc30795814c4
unknown <cogjava3103@iiht.tech>
2022-09-16 10:14:37 +0530

    new changes

--- src/main/java/com/author/controller/AuthorController.java
+++ src/main/java/com/author/controller/AuthorController.java
@@ -16,41 +16,50 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.author.model.Author;
 import com.author.service.IAuthorService;
+import com.book.dto.BookDTO;
 import com.book.model.Book;
 import com.book.service.BookService;
 
 @RestController
 @RequestMapping("/digitalbooks")
-@CrossOrigin(origins ="http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthorController {
 
 	@Autowired
-	IAuthorService authorService;
-	
+	private IAuthorService authorService;
+
 	@Autowired
-	BookService bookService;
-	
-	@PostMapping("/createbook")
-	public Long createBook(@RequestBody Book book) {
-		Long id = bookService.createBook(book);
-		return id;
-	}
+	private BookService bookService;
+
 	@PostMapping("/author/signup")
 	public Long createAuthor(@RequestBody Author author) {
 		Long id = authorService.createAccount(author);
 		return id;
 	}
+
+	@PostMapping("/createbook")
+	public Long createBook(@RequestBody Book book) {
+		Long id = bookService.createBook(book);
+		return id;
+	}
+
 	@GetMapping("/getallbooks")
-	public List<Book> getAllBooks(){
+	public List<Book> getAllBooks() {
 		return bookService.getBooks();
 	}
-	
+
 	@PutMapping("/editbook/{id}")
-	public ResponseEntity<Book> editBook(@PathVariable("id") Long id ,@RequestBody Book book ){
-		
-	return  new ResponseEntity<Book>(bookService.editBook(book, id),HttpStatus.OK);
-		
+	public ResponseEntity<Book> editBook(@PathVariable("id") Long id, @RequestBody Book book) {
+
+		return new ResponseEntity<Book>(bookService.editBook(book, id), HttpStatus.OK);
 	}
-	
-	
+
+	@GetMapping("/getbook/{category}/{price}/{authorName}")
+	public List<BookDTO> getBook(@PathVariable String category, @PathVariable Integer price,
+			@PathVariable String authorName) {
+
+		List<BookDTO> books = bookService.getBookByCategoryOrPriceOrAuthorName(category, price, authorName);
+		return books;
+
+	}
 }
